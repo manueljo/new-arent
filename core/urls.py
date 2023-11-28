fa9efccb5e92d1a3a0760991710b190bfc28b285
manueljo <50685784+manueljo@users.noreply.github.com>
2023-11-28 21:58:57 +0100

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('profile/', views.profile, name='profile'),
+    path('ussd/', views.ussd, name='ussd'),
     path('apartment/<int:pk>/', views.apartment, name='apartment-detail'),
 ]
